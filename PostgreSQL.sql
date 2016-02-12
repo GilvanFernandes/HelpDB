fdf04bec6f48c47867e423ea8ca9731043806c4a
Gilvan Fernandes <gil-f-o@hotmail.com>
2016-02-12 08:58:31 -0200

    Update - Comandos basicos do pg

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -1,1 +1,24 @@
+psql                                                                                                                 (Entrar banco)
+                    
+-l                                                                                                                   (Lista todas as bases)
+
+-U <nome-usuario>                                                                                                    (Parâmetro: Nome usuário)
+
+d                                                                                                                    (Lista tabelas, índices, sequências ou views)
+
+\d nometabela                                                                                                        (Mostra estrutura da tabela)
+
+\dt                                                                                                                  (lista tabelas)
+
+\di                                                                                                                  (Lista indices)
+
+\ds                                                                                                                  (Lista sequências)
+
+\dv                                                                                                                  (Lista views)
+
+\?                                                                                                                   (Ajuda geral dos comandos do psql)
+
+\ c nome-da-base                                                                                                     (Conectar na base)
+
+\df+                                                                                                                 (Mostrar as funcões)
 
